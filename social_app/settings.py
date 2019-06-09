836bcee8fe4aea5552601c6c0d5ff78460a65861
Piotr Chudziak <strangelove44@gmail.com>
2019-06-09 12:20:23 +0100

    Add temp folder

--- social_app/settings.py
+++ social_app/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
